14c2ef58ee9c1ea3cc892cae0831e5c5c6183bcd
fangyuwei <fangyuwei@shenhaoinfo.com>
2023-08-18 16:50:03 +0800

    feat: 修改

--- src/components/Layout/components/NavBar/index.vue
+++ src/components/Layout/components/NavBar/index.vue
@@ -8,7 +8,7 @@
     <slot name="navCenter" />
     <div class="right-menu" :style="{color: navTextColor}">
       <slot name="navRight" />
-      <!-- 自定义内容 -->123
+      <!-- 自定义内容 -->
       <el-dropdown v-if="hasAvatar" class="avatar-container" trigger="click">
         <div class="avatar-wrapper" :style="{color: navTextColor}">
           <el-avatar :src="avatar" size="small" style="margin-right:10px" />
